527792c8f1809ff95064dff478d04e9d963b3ab5
Ted Xiao <ted@ml.berkeley.edu>
2017-03-26 18:06:36 -0700

    Keras implementation of NN, not learning.

--- hw4/main.py
+++ hw4/main.py
@@ -3,6 +3,9 @@ import tensorflow as tf
 import gym
 import logz
 import scipy.signal
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, Activation, Flatten, Reshape
+from keras.utils import np_utils
 
 def normc_initializer(std=1.0):
     """
@@ -80,15 +83,57 @@ class LinearValueFunction(object):
         if self.coef is None:
             return np.zeros(X.shape[0])
         else:
-            # print("X shape: " + str(X.shape))
-            # print("coef shape: " + str(self.coef.shape))
-            test = self.preproc(X)
-            return test.dot(self.coef)
+            # print("X predict shape: " + str(X.shape)) #(2600, 3)
+            # print("X preproc predict shape: " + str(self.preproc(X).shape)) #(2600, 7)
+            # print("coef predict shape: " + str(self.coef.shape)) #(7,)
+            # print("prediction shape: " + str(self.preproc(X).dot(self.coef).shape)) # (2600, )
+            return self.preproc(X).dot(self.coef)
     def preproc(self, X):
         return np.concatenate([np.ones([X.shape[0], 1]), X, np.square(X)/2.0], axis=1)
 
 class NnValueFunction(object):
-    pass # YOUR CODE HERE
+    def __init__(self, ob_dim):
+        self.ob_dim = ob_dim
+        # print("ob dim: " + str(ob_dim))
+        # Keras Setup
+        model = Sequential()
+        model.add(Dense(32, activation='relu', input_shape=(ob_dim,)))
+        model.add(Dense(32, activation='relu'))
+        model.add(Dense(1))
+        model.compile(loss='msle', optimizer='adam')
+        model.save('nnvf.h5')
+        self.model = model
+        
+    def fit(self, X, y): # X is (2600, 3), y is (2600, ), Xp is (2600, 7)
+        # Keras training
+        model = self.model
+        model = load_model('nnvf.h5')
+        model.fit(X, y, batch_size=64, nb_epoch=10, verbose=1)
+        model.save('nnvf.h5')
+       
+        # TensorFlow Attempt
+        # observations = tf.placeholder(shape=[None, ob_dim, X.shape[1]], name='ob', dtype=tf.float32)
+        # values = tf.placeholder(shape=[None, y.shape[0]], name="val", dtype=tf.float32)
+        # h1 = lrelu(dense(observations, 32, "h1", weight_init=normc_initializer(1.0))) # hidden layer
+        # prediction = dense(h1, y.shape[0], "pred", weight_init=normc_initializer(0.1))
+        # cost = tf.reduce_mean(tf.nn.l2_loss(prediction - values))
+        # update_op = tf.train.AdamOptimizer().minimize(cost)
+        # sess = tf.Session()
+        # sess.__enter__() # equivalent to `with sess:`
+        # tf.global_variables_initializer().run() #pylint: disable=E1101
+        # _ = sess.run([update_op], feed_dict={observations:X, values:y})
+
+    def predict(self, X):
+        # Keras prediction
+        model = load_model('nnvf.h5')
+        prediction = model.predict(X)
+        # print("X predict shape: " + str(X.shape)) #(2600, 3)
+        # print("prediction shape: " + str(prediction.shape)) # (2600, )
+        # prediction = self.sess.run([prediction], feed_dict={observations:X})
+        # print("prediction shape: " + str(prediction.shape))
+        return np.squeeze(prediction)
+    def preproc(self, X):
+        return np.concatenate([np.ones([X.shape[0], 1]), X, np.square(X)/2.0], axis=1)
 
 def lrelu(x, leak=0.2):
     f1 = 0.5 * (1 + leak)
@@ -174,14 +219,14 @@ def main_cartpole(n_iter=100, gamma=1.0, min_timesteps_per_batch=1000, stepsize=
             rew_t = path["reward"]
             return_t = discount(rew_t, gamma)
             vpred_t = vf.predict(path["observation"])
-            # print("return_t shape: " + str(return_t.shape))
-            # print("vpred_t shape: " + str(vpred_t.shape))
+            print("return_t shape: " + str(return_t.shape))
+            print("vpred_t shape: " + str(vpred_t.shape))
             adv_t = return_t - vpred_t # advantage estimate! R_t - b(s_t)
             advs.append(adv_t)
             vtargs.append(return_t)
             vpreds.append(vpred_t)
 
-        print("adv_t shape: " + str(adv_t.shape))
+        print("adv_t shape: " + str(adv_t.shape)) # (200, )
         # Build arrays for policy update
         ob_no = np.concatenate([path["observation"] for path in paths])
         ac_n = np.concatenate([path["action"] for path in paths])
@@ -189,7 +234,7 @@ def main_cartpole(n_iter=100, gamma=1.0, min_timesteps_per_batch=1000, stepsize=
         # WHAT SHAPE SHOULD ADV BE?!??
         # print(adv_n.shape)
         standardized_adv_n = (adv_n - adv_n.mean()) / (adv_n.std() + 1e-8)
-        print("standardized adv shape: " + str(standardized_adv_n.shape))
+        print("standardized adv shape: " + str(standardized_adv_n.shape)) # (2600, )
         vtarg_n = np.concatenate(vtargs)
         vpred_n = np.concatenate(vpreds)
         # print(ob_no.shape)
@@ -324,8 +369,8 @@ def main_pendulum(logdir, seed, n_iter, gamma, min_timesteps_per_batch, initial_
             #Reshape
             vpred_t = vf.predict(np.squeeze(path["observation"]))
             adv_t = return_t - vpred_t
-            # print("return_t shape: " + str(return_t.shape))
-            # print("vpred_t shape: " + str(vpred_t.shape))
+            # print("return_t shape: " + str(return_t.shape)) #(200,)
+            # print("vpred_t shape: " + str(vpred_t.shape)) #(200,)
             advs.append(adv_t)
             vtargs.append(return_t)
             vpreds.append(vpred_t)
@@ -388,9 +433,9 @@ if __name__ == "__main__":
     if 1:
         general_params = dict(gamma=0.97, animate=False, min_timesteps_per_batch=2500, n_iter=300, initial_stepsize=1e-3)
         params = [
-            # dict(logdir='/tmp/ref/linearvf-kl2e-3-seed0', seed=0, desired_kl=2e-3, vf_type='linear', vf_params={}, **general_params),
-            dict(logdir='/tmp/ref/nnvf-kl2e-3-seed0', seed=0, desired_kl=2e-3, vf_type='nn', vf_params=dict(n_epochs=10, stepsize=1e-3), **general_params),
-            # dict(logdir='/tmp/ref/linearvf-kl2e-3-seed1', seed=1, desired_kl=2e-3, vf_type='linear', vf_params={}, **general_params),
+            dict(logdir='/tmp/ref/linearvf-kl2e-3-seed0', seed=0, desired_kl=2e-3, vf_type='linear', vf_params={}, **general_params),
+            # dict(logdir='/tmp/ref/nnvf-kl2e-3-seed0', seed=0, desired_kl=2e-3, vf_type='nn', vf_params=dict(n_epochs=10, stepsize=1e-3), **general_params),
+            dict(logdir='/tmp/ref/linearvf-kl2e-3-seed1', seed=1, desired_kl=2e-3, vf_type='linear', vf_params={}, **general_params),
             # dict(logdir='/tmp/ref/nnvf-kl2e-3-seed1', seed=1, desired_kl=2e-3, vf_type='nn', vf_params=dict(n_epochs=10, stepsize=1e-3), **general_params),
             # dict(logdir='/tmp/ref/linearvf-kl2e-3-seed2', seed=2, desired_kl=2e-3, vf_type='linear', vf_params={}, **general_params),
             # dict(logdir='/tmp/ref/nnvf-kl2e-3-seed2', seed=2, desired_kl=2e-3, vf_type='nn', vf_params=dict(n_epochs=10, stepsize=1e-3), **general_params),
@@ -398,4 +443,4 @@ if __name__ == "__main__":
         import multiprocessing
         p = multiprocessing.Pool()
         p.map(main_pendulum1, params)
-        # main_pendulum(None, 0, 300, 0.97, 2500, 1e-3, 2e-3, 'linear', {}, False)
+        # main_pendulum(None, 0, 300, 0.97, 2500, 1e-3, 2e-3, 'nn', {}, False)
